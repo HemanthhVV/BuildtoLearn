95818cc8c39cfc83a62dcbe45c642e45841f2f56
HEMANTHH VELLIYANGIRIE <106956442+HemanthhVV@users.noreply.github.com>
2022-10-31 21:11:11 +0530

    Add files via upload

---
+++ Week1_task.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 17:25:33 2022
+
+@author: vvhem
+"""
+'int to string'
+a=int(input())
+print(a)
+print(type(a))
+a=str(a)
+print(a)
+print(type(a))
+
+'string to int'
+b=input()
+print(b,type(b))
+print(int(b),type(int(b)))
+
+'float to int'
+c=float(input())
+print(c,type(c))
+print(int(c),type(int(c)))
+
+'str to float'
+d=input()
+print(d,type(d))
+print(float(d),type(float(d)))
+
+'int ti float'
+e=int(input())
+print(e,type(e))
+print(float(e),type(float(e)))
